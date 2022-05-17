598b2c86b8c181f0af548cfeb31205a79382abe8
DagothKur <bartosz@mystform.com>
2022-05-17 11:17:34 +0200

    add counter on alertHandler:

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState<number>(0)
+  const handlerShowAlert = () => {
+    alert("Hello React")
+    setCounter(counter + 1);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div>{`Alert was shown: ${counter} times`}</div>
+        <button onClick={handlerShowAlert}>Show Alert</button>
         <a
           className="App-link"
           href="https://reactjs.org"
